4fdb63ae7ee88cec0b827935b975fb2a55aa5d91
Sher-Shah-ss330 <ss330@rice.edu>
2024-08-22 21:59:09 -0700

    made two pages

---
+++ workload-chart/src/components/navbar.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { useGlobalState } from "./globalState";
+import { useNavigate } from "react-router";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  gap: 20px;
+  background-color: #f0f0f0;
+  box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  margin: 5px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+`;
+
+
+const Spacer = styled.div`
+  flex: 1 1 auto;
+`;
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const [globalState, setGlobalState] = useGlobalState();
+
+  const handleLogout = () => {
+      document.cookie = "user_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+      setGlobalState("user", null);
+      setGlobalState("isAuthenticated", false);
+      navigate("/login");
+      };
+
+  return (
+    <Container>
+      <Link to="/">CLA</Link>
+      <Link to="/search">Search</Link>
+
+      <Spacer></Spacer>
+
+      <Button className="button" onClick={handleLogout}>
+        Logout
+      </Button>
+    </Container>
+
+  );
+};
+
+export default Navbar;
